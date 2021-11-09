bd7be599b733f479134f8db320b03e96e0cb3a3f
Anders Hafreager <andershaf@gmail.com>
2021-11-09 05:51:22 +0100

    Cleanup

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import mixpanel from 'utils/mixpanel-frontend';
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 import './index.css';
 import App from 'App';
-import { SENTRY_DSN } from 'utils/config';
 import reportWebVitals from './reportWebVitals';
-
-Sentry.init({
-  dsn: SENTRY_DSN,
-  integrations: [new Integrations.BrowserTracing()],
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-  enabled: !window.location.hostname.includes('localhost'),
-});
 
 ReactDOM.render(
   <React.StrictMode>
