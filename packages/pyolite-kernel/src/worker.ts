06c86795e0d8176507af41802132ddf039bbf61e
Anders Hafreager <andershaf@gmail.com>
2023-01-19 16:00:51 +0100

    Added mocking of ThreadPoolExecutor to skip threading

--- packages/pyolite-kernel/src/worker.ts
+++ packages/pyolite-kernel/src/worker.ts
@@ -19,6 +19,7 @@ export class PyoliteRemoteKernel {
    **/
   async initialize(options: IPyoliteWorkerKernel.IOptions): Promise<void> {
     this._options = options;
+    console.log("Options: ", options)
 
     if (options.location.includes(':')) {
       const parts = options.location.split(':');
@@ -96,6 +97,59 @@ export class PyoliteRemoteKernel {
       await piplite.install(['pyodide-http'], keep_going=True);
       await piplite.install(['requests'], keep_going=True);
       await piplite.install(['cognite-sdk'], keep_going=True);
+
+      import functools
+      import inspect
+      from concurrent.futures import CancelledError
+      from typing import Dict
+
+      class MockFuture:
+          def __init__(self, fn, args, kwargs):
+              self._task = functools.partial(fn, *args, **kwargs)
+              self._result = None
+              self._is_cancelled = False
+
+          def result(self):
+              if self._is_cancelled:
+                  raise CancelledError
+
+              if self._result is None:  # Blocks until done
+                  self._result = self._task()
+              return self._result
+
+          def cancel(self):
+              self._is_cancelled = True
+
+
+      def mock_as_completed(dct: Dict) -> MockFuture:
+          # Will raise StopIteration when done (we want this):
+          return iter(dct.copy())
+
+
+      class MockPriorityThreadPoolExecutor:
+          def __init__(self, max_workers: int = None):
+              if max_workers != 1:
+                  raise RuntimeError("WASM says max max_workers is -e^(i*pi), or one if you wondered")
+
+          def submit(self, fn, *args, **kwargs):
+              if "priority" in inspect.signature(fn).parameters:
+                  raise TypeError(f"Given function {fn} cannot accept reserved parameter name \`priority\`")
+              kwargs.pop("priority", None)
+
+              fake_future = MockFuture(fn, args, kwargs)
+              return fake_future
+
+          def shutdown(self, wait: bool = False):
+              return None
+
+          def __enter__(self):
+              return self
+
+          def __exit__(self, type, value, traceback):
+              self.shutdown()
+
+      import os
+      os.environ["COGNITE_PATCHED"] = "3.0"
       import pyolite
       import pyodide_http
       pyodide_http.patch_all()
@@ -116,6 +170,9 @@ export class PyoliteRemoteKernel {
           self.session.mount("https://", pyodide_http._requests.PyodideHTTPAdapter())
           self.session.mount("http://", pyodide_http._requests.PyodideHTTPAdapter())
       cognite.client._http_client.HTTPClient.__init__ = new_init
+
+      cognite.client._api.datapoints.PriorityThreadPoolExecutor = MockPriorityThreadPoolExecutor
+      cognite.client._api.datapoints.as_completed = mock_as_completed
     `);
   }
 
